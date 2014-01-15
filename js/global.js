f3c2887deba9273fea3c3a07f9dcddfaa41b6380
Shaya Krauss <shaya4207@gmail.com>
2014-01-15 16:23:29 -0500

    SK initial commit

---
+++ js/global.js
@@ -0,0 +1,71 @@
+var websites = {
+  'pi-1': {
+    website_url: 'http://www.google.com',
+    website_description: 'Aenean scelerisque consectetur dui, in gravida odio venenatis ut. Maecenas placerat adipiscing felis, id posuere sem lacinia et. Nulla facilisi. Cras ut justo ut erat congue iaculis sed a ipsum. Duis dignissim laoreet blandit. Aliquam pulvinar sodales nisi sit amet ullamcorper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. In adipiscing enim id ipsum ullamcorper, eget accumsan tortor laoreet. Nunc nisl nulla, sollicitudin vitae diam sed, volutpat hendrerit nisl. Nam orci quam, consequat eget vehicula id, aliquet faucibus magna. Phasellus malesuada.',
+    website_name: 'Test Website',
+    img_url: 'http://lorempixel.com/400/200/'
+  },
+  'pi-2': {
+    website_url: 'http://www.bing.com',
+    website_description: 'Donec fermentum, purus vel sagittis eleifend, nibh ligula adipiscing quam, eget consequat dui metus eget tortor. Sed in egestas lacus, eu porta dui. Donec vel gravida orci, in lacinia quam. Pellentesque nec tempus diam. Duis arcu ipsum, ultricies eget dapibus eu, rhoncus at dui. Aliquam pharetra ante a urna imperdiet varius. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed suscipit est a lorem fringilla, sit amet pharetra diam faucibus. Sed tristique eleifend tincidunt. Proin quam orci, interdum volutpat massa.',
+    website_name: 'Test Website 2',
+    img_url: 'http://lorempixel.com/200/100/'
+  },
+  'pi-3': {
+    website_url: 'http://www.test.com',
+    website_description: 'Donec dolor ipsum, viverra at fringilla non, consequat at est. Nulla tellus turpis, mollis volutpat orci et, dignissim molestie urna. Proin in purus ligula. Maecenas porttitor in augue quis vehicula. In in risus ut turpis dictum tincidunt quis quis ipsum. Donec ipsum diam, auctor in erat non, mattis porta est. Fusce sed porta augue. Cras porta felis sit amet neque ornare, viverra scelerisque augue bibendum. Nam faucibus elit malesuada, malesuada purus sed, imperdiet purus. Aenean volutpat semper velit, vitae lacinia metus laoreet et. Nam placerat.',
+    website_name: 'Test Website',
+    img_url: 'http://lorempixel.com/600/300/'
+  },
+  'pi-4': {
+    website_url: 'http://www.example.com',
+    website_description: 'Sed purus orci, volutpat non tempor nec, viverra ut dui. Aenean tincidunt semper lacus id ornare. Fusce luctus dapibus justo, ac venenatis neque laoreet placerat. Aenean vitae purus tincidunt, tempor nibh a, ultricies lectus. Aenean dictum arcu tempus lobortis vestibulum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Fusce convallis elit viverra ullamcorper dignissim. In arcu nisl, scelerisque at arcu in, euismod lacinia augue. In hac habitasse platea dictumst. Donec turpis arcu, accumsan sit amet elit non, tempor faucibus lorem.',
+    website_name: 'Test Website 2',
+    img_url: 'http://lorempixel.com/250/125/'
+  },
+  'pi-5': {
+    website_url: 'http://www.integriaws.com',
+    website_description: 'Nullam fermentum tellus odio, dapibus tempor felis facilisis fermentum. Curabitur a orci euismod, pharetra odio sit amet, iaculis ligula. Pellentesque aliquet volutpat enim, vulputate pellentesque sem. Phasellus hendrerit, odio quis dignissim mattis, elit risus tempor diam, porta pellentesque nulla mauris eu magna. Proin aliquet commodo ante quis porta. Integer non nisi nisi. Sed fermentum augue sed nulla vulputate, sit amet dictum lectus vehicula. Aenean dignissim neque id velit rhoncus, ac sagittis justo sollicitudin. Sed quam ante, accumsan dictum massa ac, rutrum feugiat nulla. Aliquam erat.',
+    website_name: 'Test Website',
+    img_url: 'http://lorempixel.com/500/250/'
+  },
+  'pi-6': {
+    website_url: 'http://www.blah.com',
+    website_description: 'Nulla elementum ac orci sit amet imperdiet. Praesent id cursus risus. Aenean ac congue arcu. Donec faucibus ipsum rutrum vulputate auctor. Ut suscipit sollicitudin mauris, ut vulputate erat convallis sit amet. Ut porttitor orci a odio lacinia, non scelerisque tortor bibendum. Nunc sit amet cursus mi, eget scelerisque purus. Nunc vulputate aliquet turpis, id porta leo suscipit vel. Cras luctus, elit sollicitudin condimentum porttitor, sem metus vulputate arcu, sed viverra purus nisl a ante. Suspendisse potenti. Suspendisse sed mollis ligula. In in convallis est. In.',
+    website_name: 'Test Website 2',
+    img_url: 'http://lorempixel.com/450/225/'
+  },
+  'pi-7': {
+    website_url: 'http://www.yahoo.com',
+    website_description: 'Morbi ut lobortis tortor. Vivamus nec nunc porttitor, vestibulum enim sit amet, elementum mauris. Morbi non elementum massa, in mattis risus. Suspendisse mi nisl, convallis vel tempor a, commodo quis nunc. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Mauris nec rhoncus orci, quis mollis ante. Integer convallis sodales ornare. Proin in tellus dui. Praesent laoreet, metus sed scelerisque hendrerit, purus tellus sagittis nulla, vitae convallis velit purus pharetra lorem. Vestibulum porttitor tortor id lacus rutrum feugiat. Quisque placerat, sapien',
+    website_name: 'Test Website',
+    img_url: 'http://lorempixel.com/300/150/'
+  },
+  'pi-8': {
+    website_url: 'http://www.gmail.com',
+    website_description: 'Some Maecenas consectetur, felis in rutrum tincidunt, arcu leo tincidunt odio, at imperdiet est ipsum sit amet enim. Fusce sed aliquam mauris, nec interdum tortor. In a nisl dapibus, pretium orci eget, dapibus lectus. Suspendisse neque metus, pharetra lobortis lorem a, interdum luctus dolor. Sed nisi purus, ultricies ultrices nisl sit amet, congue pellentesque metus. Fusce nec sem id augue rhoncus vestibulum vitae id leo. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas sit amet rutrum sem, sit amet scelerisque.',
+    website_name: 'Test Website 2',
+    img_url: 'http://lorempixel.com/360/180/'
+  }
+}, new_lis = '';
+
+// console.log(websites.length);
+$.each(websites, function(index, el) {
+  new_lis += '<li><img src="' + el['img_url'] + '" alt="' + el['website_name'] + '" class="' + index + ' portfolio-img"></li>';
+});
+
+$('.porfolio-list').append(new_lis);
+
+$('.portfolio-img').each(function() {
+  $(this).click(function() {
+    $('.portfolio-img').removeClass('selected');
+    $(this).addClass('selected');
+
+    var thisClass = $(this).attr('class').split(' ');
+    var thisInfo = websites[thisClass[0]];
+    $('.website_name').text(thisInfo['website_name']);
+    $('.website_url').text(thisInfo['website_url']);
+    $('.website_description').text(thisInfo['website_description']);
+    $('.website_url_url').attr('href', thisInfo['website_url']);
+  });
+});
